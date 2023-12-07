5447f90bed71353f690c53fd578db759ac00fed2
Znalby <146069119+Znalby@users.noreply.github.com>
2023-12-07 08:42:55 +0700

    Add files via upload

---
+++ ddp/panggil.py
@@ -0,0 +1,26 @@
+import perhitungan
+import bangun_datar
+
+print("---perhitungan---")
+perhitungan.tambah(20,30)
+perhitungan.kurang(2,3)
+perhitungan.kali(3,2)
+perhitungan.bagi(6,2)
+perhitungan.pangkat(2,2)
+perhitungan.log(10)
+perhitungan.akar(25)
+perhitungan.sin(40)
+perhitungan.cos(40)
+perhitungan.tan(40)
+
+print("---bangun datar---")
+bangun_datar.luas_segitiga(10,5)
+bangun_datar.keliling_segitiga(10,5,5)
+bangun_datar.luas_persegi(4)
+bangun_datar.keliling_persegi(4)
+bangun_datar.luas_persegi_panjang(8,4)
+bangun_datar.keliling_persegi_panjang(8,4)
+bangun_datar.luas_belah_ketupat(5,10)
+bangun_datar.keliling_belah_ketupat(5)
+bangun_datar.luas_jajar_genjang(10,5)
+bangun_datar.keliling_jajar_genjang(5,5)
